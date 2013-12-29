155ee658b9e157972a8be216922f89b3b5aa5058
xiaochi <cumt.xiaochi@gmail.com>
2013-12-29 13:37:26 +0800

    run gui

--- tk.py
+++ tk.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
 
-import Tkinter
+import Tkinter # sudo apt-get install python-tk
+import svnlib
+
 root = Tkinter.Tk()
 
-# names
-
-keyword_entry = None
-
-def layout_top(root):
+def layout_root(root):
     keyword_entry = Tkinter.Entry(root)
     keyword_entry.pack(side = Tkinter.TOP)
 
     text = Tkinter.Text(root)
-    text.insert(Tkinter.INSERT, "Hello.....")
-    text.insert(Tkinter.END, "Bye Bye.....")
     text.pack(side = Tkinter.BOTTOM)
 
-    text.tag_add("here", "1.0", "1.2")
-    text.tag_add("start", "1.8", "1.13")
-    text.tag_config("here", background="yellow", foreground="blue")
-    text.tag_config("start", background="black", foreground="green")
+    return [keyword_entry, text]
 
+[keyword_entry, text] = layout_root(root)
 
-layout_top(root)
+root_url = 'svn://svn.fangdd.net/fdd-web'
+logs = svnlib.search(root_url, 'wangxiaochi')
+i = 0
+for log in logs:
+    text.insert(Tkinter.END, log+'\n\n')
+print len(logs)
+
+text.tag_add("here", "1.0", "1.2")
+text.tag_add("start", "1.8", "1.13")
+text.tag_config("here", background="yellow", foreground="blue")
+text.tag_config("start", background="black", foreground="green")
 
 root.mainloop()
 
