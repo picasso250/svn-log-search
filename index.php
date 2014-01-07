7f325d03e468326e5f4d81a3bbaf2efd86a80595
xiaochi <cumt.xiaochi@gmail.com>
2014-01-07 14:28:28 +0800

    start queue when svn log donot exist

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 
+require 'vender/autoload.php';
 require 'lib.php';
+$config = include 'config.php';
+
+$root_url = $config[0];
+
+$fpath = get_log_file_name($root_url);
+if (!file_exists($fpath)) {
+
+    // Required if redis is located elsewhere
+    Resque::setBackend('localhost:6379');
+
+    $args = array(
+            'url' => $root_url,
+            );
+    Resque::enqueue('default', 'SvnUpdateLogJob', $args);
+
+    die('we are updating, please come back later');
+
+}
 
 $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
-$root_url = isset($_GET['root_url']) ? $_GET['root_url'] : '';
-if ($root_url) {
-    $keywords = array_filter(explode(' ', trim($keyword)), 'trim');
-    $logs = search($keywords, $root_url);
-}
+$keywords = array_filter(explode(' ', trim($keyword)), 'trim');
+$logs = search($keywords, $root_url);
 
 include 'index.phtml';
