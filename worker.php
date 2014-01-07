3b859cf21a8c2949150a5d452990236afd8d1759
xiaochi <cumt.xiaochi@gmail.com>
2014-01-07 20:39:01 +0800

    config db

--- worker.php
+++ worker.php
@@ -1,6 +1,11 @@
 <?php
 
+require 'vendor/autoload.php';
 require 'lib.php';
 require 'SvnUpdateLogJob.php';
+$config = include 'config.php';
+
+ORM::configure($config['dsn']);
+ORM::configure($config['db_config']);
 
 include 'vendor/chrisboulton/php-resque/resque.php';
