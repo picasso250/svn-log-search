f4caa13d70873fc1f4b44e786c3bccd8716fdaea
xiaochi <cumt.xiaochi@gmail.com>
2014-01-07 16:04:42 +0800

    add require

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 
-require 'vender/autoload.php';
+require 'vendor/autoload.php';
 require 'lib.php';
+require 'SvnUpdateLogJob.php';
 $config = include 'config.php';
 
 $root_url = $config[0];
 
 $fpath = get_log_file_name($root_url);
 if (!file_exists($fpath)) {
-
-    // Required if redis is located elsewhere
-    Resque::setBackend('localhost:6379');
-
     $args = array(
             'url' => $root_url,
             );
-    Resque::enqueue('default', 'SvnUpdateLogJob', $args);
-
+    $token = Resque::enqueue('default', 'SvnUpdateLogJob', $args);
     die('we are updating, please come back later');
-
 }
 
 $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
