3e6f75020ac83db6892cf7ddc8c3279c574850d0
xiaochi <cumt.xiaochi@gmail.com>
2014-01-07 20:38:41 +0800

    search in db

--- index.php
+++ index.php
@@ -5,20 +5,23 @@ require 'lib.php';
 require 'SvnUpdateLogJob.php';
 $config = include 'config.php';
 
+ORM::configure($config['dsn']);
+ORM::configure($config['db_config']);
+
 $repo = isset($_GET['repo']) ? $_GET['repo'] : 0;
 $root_url = $config['repos'][$repo];
 
-$fpath = get_log_file_name($root_url);
-if (!file_exists($fpath)) {
+// $fpath = get_log_file_name($root_url);
+// if (!file_exists($fpath)) {
     $args = array(
             'url' => $root_url,
             );
     $token = Resque::enqueue('default', 'SvnUpdateLogJob', $args);
-    die('we are updating, please come back later');
-}
+    // die('we are updating, please come back later');
+// }
 
 $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
 $keywords = array_filter(explode(' ', trim($keyword)), 'trim');
-$logs = search($keywords, $root_url);
+$logs = search_db($keywords, $root_url);
 
 include 'index.phtml';
