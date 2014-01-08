2c098499a25d5208f67f96bee4f1459ed4576c7b
xiaochi <cumt.xiaochi@gmail.com>
2014-01-08 14:14:51 +0800

    add diff

---
+++ diff.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'lib.php';
+require 'SvnGetDiffJob.php';
+$config = include 'config.php';
+
+ORM::configure($config['dsn']);
+ORM::configure($config['db_config']);
+
+if (!isset($_GET['repo']) || !isset($_GET['file']) || !isset($_GET['revision'])) {
+    die('no repo or no file or no revision given');
+}
+
+$repo = isset($_GET['repo']) ? $_GET['repo'] : 0;
+$file = $_GET['file'];
+$revision = $_GET['revision'];
+
+$diff = get_diff_from_db($repo, $file, $revision);
+if (empty($diff)) {
+    $args = array(
+            'url' => $repo,
+            'file' => $file,
+            'revision' => $revision,
+            );
+    $token = Resque::enqueue('default', 'SvnGetDiffJob', $args);
+    die('we are get diff, please come back later');
+}
+
+include 'diff.phtml';
