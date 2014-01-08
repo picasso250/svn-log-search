b5ae93afdaa62ad789e09d6617c4d53c6afc4084
xiaochi <cumt.xiaochi@gmail.com>
2014-01-08 11:43:34 +0800

    符合范式啊

--- install.sql
+++ install.sql
@@ -1,18 +1,42 @@
 
 -- ----------------------------
+-- Table structure for blame
+-- ----------------------------
+DROP TABLE IF EXISTS `blame`;
+CREATE TABLE `blame` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rev_id` int(10) unsigned DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `blame` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for changed_path
 -- ----------------------------
 DROP TABLE IF EXISTS `changed_path`;
 CREATE TABLE `changed_path` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `rev` int(10) unsigned DEFAULT NULL,
+  `rev_id` int(10) unsigned DEFAULT NULL,
   `text_mods` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `kind` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `action` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `prop_mods` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `file_path` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `path_rev_idx` (`rev`)
+  KEY `path_rev_idx` (`rev_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for diff
+-- ----------------------------
+DROP TABLE IF EXISTS `diff`;
+CREATE TABLE `diff` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rev_id` int(10) unsigned DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `diff` blob,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -30,34 +54,12 @@ CREATE TABLE `repo` (
 -- ----------------------------
 DROP TABLE IF EXISTS `rev`;
 CREATE TABLE `rev` (
-  `rev` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rev` int(10) unsigned NOT NULL,
   `repo_id` int(10) unsigned DEFAULT NULL,
   `author` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `commit_date` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   `line_num` smallint(6) DEFAULT NULL,
   `msg` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  PRIMARY KEY (`rev`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-delimiter $$
-
-CREATE TABLE `diff` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `repo_id` int(10) unsigned DEFAULT NULL,
-  `rev` int(10) unsigned DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
-  `diff` blob,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
-delimiter $$
-
-CREATE TABLE `blame` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `repo_id` int(10) unsigned DEFAULT NULL,
-  `rev` int(10) unsigned DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
-  `blame` blob,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
